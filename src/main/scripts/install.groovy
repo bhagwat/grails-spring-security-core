712d06b5444088f64b83cee1154e6df95165e277
Bhagwat Kumar <bhagwat@intelligrape.com>
2015-04-20 04:52:49 +0530

    Migrated main plugin including package imports, new file structure and scripts

---
+++ src/main/scripts/install.groovy
@@ -0,0 +1,11 @@
+description "Install plugin", "grails install plugin spring-security-core"
+
+println '''
+*******************************************************
+* You've installed the Spring Security Core plugin.   *
+*                                                     *
+* Next run the "s2-quickstart" script to initialize   *
+* Spring Security and create your domain classes.     *
+*                                                     *
+*******************************************************
+'''
